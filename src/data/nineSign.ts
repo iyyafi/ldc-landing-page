b5b2892d5cddd52d29d4cfe629983c56e8b05f8e
Iyyafi Qolbi <iyyafi@gmail.com>
2024-01-03 15:58:18 +0700

    add 9 sign

---
+++ src/data/nineSign.ts
@@ -0,0 +1,11 @@
+export const nineSign = [
+    'Tidak mobile responsive',
+    'Loading website lambat',
+    'Tidak user-friendly',
+    'Tidak SEO-friendly',
+    'Desain website terlihat jadul',
+    'Terdapat error dan bugs',
+    'Memiliki sedikit pengunjung',
+    'Konversi rendah',
+    'Banyak komentar negatif',
+]

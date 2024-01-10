beeaa0aa00c1f5ba0b27480e5ec1f3f4b0c82b10
Iyyafi Qolbi <iyyafi@gmail.com>
2024-01-10 09:50:32 +0700

    some update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/functions'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: netlify()
-});
+    integrations: [tailwind(), icon()],
+    output: 'server',
+    adapter: netlify(),
+})
